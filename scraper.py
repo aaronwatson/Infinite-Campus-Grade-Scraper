a1cc07d37a66101ee7c73cb031d57e77318bff09
Aaron Watson <aaron@AppleMDM.local>
2018-02-07 20:38:53 -0800

    Fixed error when 'student_index' is not present in ENV

--- scraper.py
+++ scraper.py
@@ -116,9 +116,7 @@ def setup():
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 def get_student_index():
-    if os.environ['student_index']:
-        return int(os.environ['student_index'])
-    return 0
+    return int(os.getenv('student_index', '1'))
 
 def get_base_url():
     """returns the site's base url, taken from the login page url"""
